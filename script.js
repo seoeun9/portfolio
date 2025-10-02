21a9075e302b33ad9360f41c502c46ab8ad06c63
seoeun9 <169060897+seoeun9@users.noreply.github.com>
2025-10-02 16:49:57 +0900

    add structure

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('.text');
+const content = "Hi, I'm Seoeun.";
+const cursor = document.querySelector('.blink');
+let i = 0;
+let speed = 200;
+
+function typing() {
+    if (i < content.length) {
+        let txt = content.charAt(i);
+        text.innerHTML += txt;
+        i++;
+    }
+    else{
+        cursor.style.display = 'none';
+    }
+}
+setInterval(typing, speed);

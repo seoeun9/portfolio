64708c1f5cedd5c7341e47716b41822c27477e13
seoeun9 <169060897+seoeun9@users.noreply.github.com>
2025-11-20 17:55:51 +0900

    add basic layout

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const text = document.querySelector('.text');
 const content = "Hi, I'm Seoeun.";
 const cursor = document.querySelector('.blink');
 let i = 0;
-let speed = 200;
+let speed = 75;
 
 function typing() {
     if (i < content.length) {
@@ -17,3 +17,35 @@ function typing() {
     }
 }
 setInterval(typing, speed);
+
+const faders = document.querySelectorAll('.fade');
+
+const options = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, options);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+const headerText = document.querySelector('.header-text');
+const navBar = document.querySelector('.nav-bar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        headerText.classList.add('hide');      // 텍스트 사라짐
+        navBar.classList.add('scroll-up');     // 네비 위로
+    } else {
+        headerText.classList.remove('hide');
+        navBar.classList.remove('scroll-up');
+    }
+});

35f772d01b974c32cdbf5f752d4df1863f5dbc5a
장서은 <seoeun9@naver.com>
2026-08-18 19:42:52 +0900

    fix(fe): delete next dev setting

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : undefined,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
